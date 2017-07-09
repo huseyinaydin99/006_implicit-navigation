e425d1760758524b4d5d905918985cfae9dd3d12
huso51 <huseyinaydin99@gmail.com>
2017-07-09 19:57:14 +0300

    Add files via upload

---
+++ 006_Implicit_Navigation/src/java/com/huseyinaydin/Sayfa2Controller.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.huseyinaydin;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author husey
+ */
+public class Sayfa2Controller implements Serializable{
+
+    /**
+     * Creates a new instance of Sayfa2Controller
+     */
+    public Sayfa2Controller() {
+    }
+    
+    public String sayfa2yiAc(){
+        return "sayfa2";
+    }
+    
+}
